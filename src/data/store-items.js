58eb2868a6e677d7a1769bfaba3ba004363b160c
Cesar Ticona <cesarju.cc@gmail.com>
2023-01-26 18:36:18 -0400

    list store

---
+++ src/data/store-items.js
@@ -0,0 +1,37 @@
+const listStore = [
+  {
+    id: 1,
+    name: "Julius K9 Arnés Powair Verano Azul",
+    img: "https://static.miscota.com/media/1/photos/products/174972/174972-20pa-b-m_2_g.jpg",
+    categoria: "Collares",
+    precio: 50,
+  },
+  {
+    id: 2,
+    name: "Grature Pienso Puppy de Pollo para Cachorros",
+    img: "https://static.miscota.com/media/1/photos/products/500798/grature-puppy-chicken-seco-6368e53b8d830_g.jpg",
+    categoria: "Comida",
+    precio: 200,
+  },
+  {
+    id: 3,
+    name: "Grature Comida Húmeda de Pollo en Paté para Perros",
+    img: "https://static.miscota.com/media/1/photos/products/502351/grature-wet-dog-chicken-6368e51aa0102_g.jpg",
+    categoria: "Comida",
+    precio: 130,
+  },
+  {
+    id: 4,
+    name: "Zanahoria con Sonido",
+    img: "https://static.miscota.com/media/1/photos/products/423378/8435117831599_g.jpg",
+    categoria: "Juguetes",
+    precio: 100,
+  },
+  {
+    id: 5,
+    name: "Julius K9 Collar Color & Gray con Asa Rosa",
+    img: "https://static.miscota.com/media/1/photos/products/427032/collar-juliusk9-para-perro-62dfd55e8330c_g.jpg",
+    categoria: "Collares",
+    precio: 10,
+  },
+];
